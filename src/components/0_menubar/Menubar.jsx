b96e16cfecac7d9f080c16a410ab490122d73004
Yi Ting Wang <50982563+AmberWow123@users.noreply.github.com>
2021-05-19 03:49:20 -0700

    Added phone# and email on menu bar

--- src/components/0_menubar/Menubar.jsx
+++ src/components/0_menubar/Menubar.jsx
@@ -1,4 +1,7 @@
 import "./menubar.scss"
+// import PersonIcon from '@material-ui/icons/Person';
+// import MailIcon from '@material-ui/icons/Mail';
+import {Person, Mail} from "@material-ui/icons"
 
 export default function Menubar() {
     return (
@@ -6,10 +9,19 @@ export default function Menubar() {
             <div className="wrapper">
                 <div className="left">
                     {/* make a link for such that each click will return back to the intro page */}
-                    <a href="#intro">asdasdasd</a>
+                    <a href="#intro" className="logo">Eatingwow</a>
+                    <div className="itemContainer">
+                        <Person className="icon"/>
+                        <span>+886 9 56816556</span>
+                    </div>
+                    <div className="itemContainer">
+                        <Mail className="icon"/>
+                        <span>eatingwow@gmail.com</span>
+                    </div>
                 </div>
+                <h1>Hello</h1>
                 <div className="right">
-
+                    this is right
                 </div>
             </div>
         </div>
