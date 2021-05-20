00a9a2d836d10e384f1e29d87e6c59f9fe424956
Yi Ting Wang <50982563+AmberWow123@users.noreply.github.com>
2021-05-20 02:44:01 -0700

    Finished side menu

--- src/components/0.5_sidemenu/SideMenu.jsx
+++ src/components/0.5_sidemenu/SideMenu.jsx
@@ -4,19 +4,22 @@ export default function SideMenu({menuOpen, setMenuOpen}) {
     return (
         <div className={"sideMenu " + (menuOpen && "active")}>
             <ul>
-                <li>
+                {/* 'onClick={()=>setMenuOpen(false)}'
+                    will close the side menu when that section
+                    is clicked */}
+                <li onClick={()=>setMenuOpen(false)}>
                     <a href="#intro">Home</a>
                 </li>
-                <li>
+                <li onClick={()=>setMenuOpen(false)}>
                     <a href="#skills">Skills</a>
                 </li>
-                <li>
+                <li onClick={()=>setMenuOpen(false)}>
                     <a href="#materials">Materials</a>
                 </li>
-                <li>
+                <li onClick={()=>setMenuOpen(false)}>
                     <a href="#projects">Projects</a>
                 </li>
-                <li>
+                <li onClick={()=>setMenuOpen(false)}>
                     <a href="#contact">Contact</a>
                 </li>
             </ul>
