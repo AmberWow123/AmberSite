e055896514d865ae10d50ff79af00af399107e43
Yi Ting Wang <50982563+AmberWow123@users.noreply.github.com>
2021-05-20 11:59:40 -0700

    Create data.js
    Data for About section
    (about me, education, experience)

---
+++ src/components/data.js
@@ -0,0 +1,43 @@
+export const aboutmeData = [
+    {
+        id:1,
+        title: "Taiwan",
+        img: "https://live.staticflickr.com/3583/3509815275_4559fee0d5_b.jpg",
+    },
+    {
+        id:2,
+        title: "Shanghai",
+        img: "https://th.bing.com/th/id/R3863b48e39bec5088b17ab9e9393bcd0?rik=As%2f9KzDw8sNYeg&pid=ImgRaw",
+    },
+];
+
+export const educationData = [
+    {
+        id:1,
+        title: "college",
+        img: "https://is2-ssl.mzstatic.com/image/thumb/Purple114/v4/25/f3/14/25f31425-4837-2a9b-736e-dba6d0a0d058/source/512x512bb.jpg",
+    },
+    {
+        id:2,
+        title: "undergrad",
+        img: "https://th.bing.com/th/id/R1921a746eec1e5595b0c3e1d127100ba?rik=60rpoS%2fL9v3m4w&pid=ImgRaw",
+    },
+    {
+        id:3,
+        title: "grad",
+        img: "https://th.bing.com/th/id/R1921a746eec1e5595b0c3e1d127100ba?rik=60rpoS%2fL9v3m4w&pid=ImgRaw",
+    },
+];
+
+export const experienceData = [
+    {
+        id:1,
+        title: "mathTutor",
+        img: "https://www.deanza.edu/autotech/images/logo-scs.gif",
+    },
+    {
+        id:2,
+        title: "csTutor",
+        img: "https://www.deanza.edu/autotech/images/logo-scs.gif",
+    },
+];
