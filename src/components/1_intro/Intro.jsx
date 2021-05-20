e61085ad7333e02e491c135022b99e200296d998
Yi Ting Wang <50982563+AmberWow123@users.noreply.github.com>
2021-05-20 04:22:36 -0700

    Finished intro section

--- src/components/1_intro/Intro.jsx
+++ src/components/1_intro/Intro.jsx
@@ -1,9 +1,26 @@
 import "./intro.scss"
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 export default function Intro() {
     return (
         <div className="intro" id="intro">
-            asdasd
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="assets/intro_photo.jpg" alt=""></img>
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>UCSD Computer Science Undergrad</h2>
+                    <h1>Yi-Ting, Wang</h1>
+                    <h3>sdasdasd <span>ABC</span></h3>
+                </div>
+                <a href="#skills">
+                    <ExpandMoreIcon className="icon"/>
+                    {/* use image as a button to go to project section */}
+                    {/* <img arc="assets/xxx.jpg" alt=""></img> */}
+                </a>
+            </div>
         </div>
     )
 }
