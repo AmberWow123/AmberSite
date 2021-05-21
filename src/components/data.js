22633e1acdef99d2f89d468f7bffb57b664596aa
Yi Ting Wang <50982563+AmberWow123@users.noreply.github.com>
2021-05-20 22:03:07 -0700

    Update data.js

--- src/components/data.js
+++ src/components/data.js
@@ -3,21 +3,33 @@ export const locationData = [
         id:1,
         title: "Taiwan",
         img: "https://live.staticflickr.com/3583/3509815275_4559fee0d5_b.jpg",
+        modalImg: "",
+        modalTitle: "Born at Taiwan",
+        modalContent: "Lived in Tainan",
     },
     {
         id:2,
         title: "Shanghai",
         img: "https://th.bing.com/th/id/R3863b48e39bec5088b17ab9e9393bcd0?rik=As%2f9KzDw8sNYeg&pid=ImgRaw",
+        modalImg: "",
+        modalTitle: "6yo-8yo",
+        modalContent: "Lived in Shanghai 1.5 years",
     },
     {
         id:3,
         title: "Cupertino",
         img: "https://g5-assets-cld-res.cloudinary.com/image/upload/x_0,y_261,h_1210,w_2200,c_crop/q_auto,f_auto,c_fill,g_center,h_1100,w_2000/v1569524057/g5/g5-c-iqbdxrhy-acco-management-co-client/g5-cl-57b5vdvf2-glenbrook/uploads/IMG_1391a_1_djgoeb.jpg",
+        modalImg: "",
+        modalTitle: "2016-2019",
+        modalContent: "For college",
     },
     {
         id:4,
         title: "San Diego",
         img: "https://cdn.turkishairlines.com/m/b4ff11fdcdb737c/original/1400-660-jpg.jpg",
+        modalImg: "",
+        modalTitle: "2019-current",
+        modalContent: "For University",
     },
 ];
 
@@ -26,16 +38,25 @@ export const educationData = [
         id:1,
         title: "College",
         img: "https://is2-ssl.mzstatic.com/image/thumb/Purple114/v4/25/f3/14/25f31425-4837-2a9b-736e-dba6d0a0d058/source/512x512bb.jpg",
+        modalImg: "",
+        modalTitle: "Transfer",
+        modalContent: "",
     },
     {
         id:2,
         title: "Undergraduate",
         img: "https://th.bing.com/th/id/R1921a746eec1e5595b0c3e1d127100ba?rik=60rpoS%2fL9v3m4w&pid=ImgRaw",
+        modalImg: "",
+        modalTitle: "BS Degree",
+        modalContent: "",
     },
     {
         id:3,
         title: "Graduate",
         img: "https://th.bing.com/th/id/R1921a746eec1e5595b0c3e1d127100ba?rik=60rpoS%2fL9v3m4w&pid=ImgRaw",
+        modalImg: "",
+        modalTitle: "Master degree",
+        modalContent: "",
     },
 ];
 
@@ -44,11 +65,17 @@ export const experienceData = [
         id:1,
         title: "Math Tutor",
         img: "https://www.deanza.edu/autotech/images/logo-scs.gif",
+        modalImg: "",
+        modalTitle: "De Anza College - Student Success Center",
+        modalContent: "",
     },
     {
         id:2,
         title: "CS Tutor",
         img: "https://www.deanza.edu/autotech/images/logo-scs.gif",
+        modalImg: "",
+        modalTitle: "De Anza College - Computer Lab",
+        modalContent: "",
     },
 ];
 
@@ -57,5 +84,8 @@ export const achievementData = [
         id:1,
         title: "Summa Cum Laude",
         img: "https://images.squarespace-cdn.com/content/v1/543c8c93e4b03ff31dc5cd8d/1495410751579-7332AJX22ZL5NCZ54F93/ke17ZwdGBToddI8pDm48kLkXF2pIyv_F2eUT9F60jBl7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0iyqMbMesKd95J-X4EagrgU9L3Sa3U8cogeb0tjXbfawd0urKshkc5MgdBeJmALQKw/IMG_0040.jpg?format=750w",
+        modalImg: "",
+        modalTitle: "Graduation Honor",
+        modalContent: "Top 2 percent",
     },
 ];
