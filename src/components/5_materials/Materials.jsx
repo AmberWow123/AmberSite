dea9238f512d400bb2a4c8e8784a9a0e57643288
Yi Ting Wang <50982563+AmberWow123@users.noreply.github.com>
2021-05-23 12:10:03 -0700

    Finished right half of Materials section

--- src/components/5_materials/Materials.jsx
+++ src/components/5_materials/Materials.jsx
@@ -4,6 +4,17 @@ export default function Materials() {
     return (
         <div className="materials" id="materials">
             materials
+
+            <div className="left">
+
+            </div>
+            <div className="right">
+                <iframe 
+                    src="assets/DA_official_transcript.pdf">
+                    className="viewer"
+                </iframe>
+            </div>
+            
         </div>
     )
 }
