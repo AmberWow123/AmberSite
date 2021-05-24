bde85b34a50735a5936bc5ebf449fa8bf38680fa
Yi Ting Wang <50982563+AmberWow123@users.noreply.github.com>
2021-05-24 01:20:14 -0700

    Updated format structure of Materials section

--- src/components/5_materials/Materials.jsx
+++ src/components/5_materials/Materials.jsx
@@ -3,16 +3,18 @@ import "./materials.scss"
 export default function Materials() {
     return (
         <div className="materials" id="materials">
-            materials
-
             <div className="left">
 
             </div>
-            <div className="right">
+            <div className="middle">
                 <iframe 
                     src="assets/DA_official_transcript.pdf">
                     className="viewer"
                 </iframe>
+            </div>
+
+            <div className="right">
+                
             </div>
             
         </div>
